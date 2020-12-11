2b95ddb7a06399ae20c30b6226643ea4c9c5e0b7
tejal-bhavsar-au6 <tejalbhavsar02@gmail.com>
2020-12-12 01:54:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{ useEffect, useState } from 'react';
 
-function App() {
+function App() { 
+
+  const APIKEY = "49575309fc1b87cac117983a776b57e5";
+  const [ weather, setWeather ] = useState({})
+  const [ city, setCity ] = useState('delhi')
+  const [ search , setSearch ] = useState('')
+  useEffect(() => {
+    getWeather()
+  },[city])
+
+   
+
+  const getWeather = async () => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKEY}`)
+    const data =  await response.json()
+    console.log('here', data)
+    setWeather(data)
+    console.log(weather,'smzana')
+  }
+  
+  const getSearch = (e) => {
+    e.preventDefault()
+    setCity(search)
+    setSearch('')
+    
+  }
+
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="maindiv">
+       <form onSubmit={getSearch}>
+        <input className="searchinput"
+        placeholder="search" 
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        />
+        <button className="search-btn" type="submit" >search</button>
+       </form>
+
+      {  (Object.entries(weather).length != 0)? 
+
+        <div className="weather-wrap">
+          <div className="location-box">
+            <div className="location">{weather.name}<p>{weather.sys.country}</p></div>
+            
+            <div className="date">date {} </div>
+            
+
+            <div className="weather-box">
+              <div className="temp"> {weather.main.temp}°c</div>
+
+              <div className="weather">
+                <p>{weather.weather[0].main}</p>
+                <p>{weather.weather[0].description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        : "enter the city name"
+      }
+
+     </div>
     </div>
   );
 }
